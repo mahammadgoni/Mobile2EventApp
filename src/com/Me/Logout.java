7a7c5aa3da70e552c63c7171afac63327d9dd0ee
Goni <goni@github.com>
2018-06-06 17:51:28 +0530

    Notes Changes

---
+++ src/com/Me/Logout.java
@@ -0,0 +1,123 @@
+package com.Me;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.BaseSetup.BaseSetUp;
+
+public class Logout extends BaseSetUp{
+	
+	
+	By emailId = By.id("ws.e2m.main:id/et_email");
+	
+	By pass = By.id("ws.e2m.main:id/et_password");
+	
+	By proceedBtn1 = By.id("ws.e2m.main:id/tv_proceed");
+	
+	By proceedBtn2 = By.id("ws.e2m.main:id/rl_proceed");
+	
+	By submitBtn = By.id("ws.e2m.main:id/tv_submit");
+	
+	By menu = By.id("ws.e2m.main:id/btn_home");
+	
+//	Clicking On My Profile Elements
+	
+	By clickOnMe = By.xpath("//*[@content-desc='Me']");
+	
+	By clickOnLogout = By.xpath("//*[@content-desc='Log out']");
+	
+	By yesBtn = By.id("ws.e2m.main:id/tv_yes");
+	
+
+	public Logout(WebDriver driver) {
+		super(driver);
+		
+	}
+	
+//	Common Login Method
+	
+	public void commonActivity(String userName,String password) throws InterruptedException{
+		
+		System.out.println("Clicking on Your Email ");
+
+		waitForClickabilityOf(emailId);
+
+		driver.findElement(emailId).clear();
+
+		System.out.println("Entering the Email  :" + userName);
+
+		driver.findElement(emailId).sendKeys(userName);
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Clicking on Proceed Button ");
+
+		waitForClickabilityOf(proceedBtn1);
+
+		driver.findElement(proceedBtn1).click();
+		
+		Thread.sleep(2000);
+
+		System.out.println("Entering the Pin  :" + password);
+
+		waitForClickabilityOf(pass);
+		
+		driver.findElement(pass).clear();
+
+		driver.findElement(pass).sendKeys(password);
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Clicking on Proceed Button ");
+
+		waitForClickabilityOf(proceedBtn2);
+
+		driver.findElement(proceedBtn2).click();
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Clicking on Menu Option ");
+
+		waitForClickabilityOf(menu);
+
+		driver.findElement(menu).click();
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Clicking on Me ");
+
+		waitForClickabilityOf(clickOnMe);
+
+		driver.findElement(clickOnMe).click();
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Clicking on Logout");
+
+		waitForClickabilityOf(clickOnLogout);
+
+		driver.findElement(clickOnLogout).click();
+		
+	}
+	
+	public Logout logoutCheck(String userName,String password) throws InterruptedException{
+		
+		commonActivity(userName, password);
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Clicking on Yes Button Logout");
+
+		waitForClickabilityOf(yesBtn);
+
+		driver.findElement(yesBtn).click();
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Successfully Logout from Account");
+		
+		
+		return new Logout(driver);
+	}
+
+}
